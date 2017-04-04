93f6a7953ff4f09eeab070c957cc7c31c87c5b3b
Vitrio <vitrio@noxus.co.id>
2017-04-04 07:53:31 +0000

    Fixed seeder is not working on postgresql

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        Users::create([
             'name' => str_random(10),
             'email' => str_random(10).'@gmail.com',
             'password' => bcrypt('secret'),
