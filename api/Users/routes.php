b5520a7f39a98825fea6a4be4d2d6bbaa7aea821
Vitrio <vitrio@noxus.co.id>
2017-04-08 10:10:38 +0000

    Added HTTP PATCH to the update data partially

--- api/Users/routes.php
+++ api/Users/routes.php
@@ -1,7 +1,9 @@
+
 <?php
 
 $router->get('/users', 'UserController@getAll');
 $router->get('/users/{id}', 'UserController@getById');
 $router->post('/users', 'UserController@create');
 $router->put('/users/{id}', 'UserController@update');
+$router->patch('/users/{id}', 'UserController@update');
 $router->delete('/users/{id}', 'UserController@delete');
